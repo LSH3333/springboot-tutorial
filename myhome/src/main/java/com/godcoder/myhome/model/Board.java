5f421db3190c6d7e9556bdfefcfcaf9a2d7e9da8
LSH <dltpgustpgus@gmail.com>
2022-08-09 16:14:45 +0900

    db, list.html

---
+++ myhome/src/main/java/com/godcoder/myhome/model/Board.java
@@ -0,0 +1,19 @@
+package com.godcoder.myhome.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data // getter setter
+public class Board
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // db
+    private Long id;
+    private String title;
+    private String content;
+}
